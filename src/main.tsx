79ce564dd18c2561c2a32ad625db8918f014bcbc
Akanni Oluwafemi <akannibenjamin200@gmail.com>
2026-08-19 20:57:10 +0100

    fix theme stylesheet import case

--- src/main.tsx
+++ src/main.tsx
@@ -7,7 +7,7 @@ import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 
 import "./styles/global.css";
-import "./styles/Theme.css";
+import "./styles/theme.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
